5a7205fae57490ecb60bdf5a1a9c086c7a2b3723
auttenbergo <gigi.gvaramia@gmail.com>
2021-12-08 12:56:05 +0400

    Refactored generate-model and excluded parser functions to template-parsers

--- generate-model.sh
+++ generate-model.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+# Based on the user given project path, returns the package name excluded. Package name is the path after /java directory
+
 get_package_name () {
     RESULT=''
     PACKAGE="${PROJECT_PATH}"
@@ -12,8 +15,7 @@ get_package_name () {
     echo "${RESULT}"
 }
 
-
-readonly SCRIPT_PATH=$(dirname "${0}")
+readonly SCRIPT_PATH=$(dirname "${0}") ; export SCRIPT_PATH
 readonly MODEL="${1}"
 readonly PROJECT_PATH="${2}"
 readonly PACKAGE=$(get_package_name)
@@ -27,187 +29,125 @@ readonly MAPPER="${MODEL}Mapper";
 readonly ADD_PARAM="Add${MODEL}Param"
 readonly UPDATE_PARAM="Update${MODEL}Param"
 
-# FUNCTIONS FOR CONTROLLER
+# Get Exported Parser Functions
 
-get_controller_parsed () {
-    local CONTROLLER_TEMPLATE=$(/usr/bin/cat "${SCRIPT_PATH}"/templates/controller-template)
-    CONTROLLER_TEMPLATE=$(echo "${CONTROLLER_TEMPLATE//\{PACKAGE\}/${PACKAGE}}")
-    API_BASE_PATH=$(echo ${MODEL,} | sed -r 's/([a-z0-9])([A-Z])/\1-\L\2/g')
-    CONTROLLER_TEMPLATE=$(echo "${CONTROLLER_TEMPLATE//\{API_BASE_PATH\}/${API_BASE_PATH}}")
-    CONTROLLER_TEMPLATE=$(echo "${CONTROLLER_TEMPLATE//\{DTO_CLASS_NAME\}/$DTO}")
-    CONTROLLER_TEMPLATE=$(echo "${CONTROLLER_TEMPLATE//\{DTO_VAR_NAME\}/${DTO,}}")
-    CONTROLLER_TEMPLATE=$(echo "${CONTROLLER_TEMPLATE//\{CONTROLLER_CLASS_NAME\}/$CONTROLLER}")
-    CONTROLLER_TEMPLATE=$(echo "${CONTROLLER_TEMPLATE//\{FACADE_CLASS_NAME\}/$FACADE}")
-    CONTROLLER_TEMPLATE=$(echo "${CONTROLLER_TEMPLATE//\{FACADE_VAR_NAME\}/${FACADE,}}")
-    CONTROLLER_TEMPLATE=$(echo "${CONTROLLER_TEMPLATE//\{MODEL_NAME\}/$MODEL}")
-    CONTROLLER_TEMPLATE=$(echo "${CONTROLLER_TEMPLATE//\{MODEL_NAME_LOWER\}/${MODEL,}}")
-    CONTROLLER_TEMPLATE=$(echo "${CONTROLLER_TEMPLATE//\{UPDATE_MODEL_PARAM_CLASS_NAME\}/$UPDATE_PARAM}")
-    CONTROLLER_TEMPLATE=$(echo "${CONTROLLER_TEMPLATE//\{ADD_MODEL_PARAM_CLASS_NAME\}/$ADD_PARAM}")
+source ./template-parsers.sh
 
-    echo "${CONTROLLER_TEMPLATE}"
+
+# Function which creates the ${1} and writes in ${1} the value of ${2}
+create_and_write_file () {
+    touch "${1}"
+    echo "${2}" > "${1}"
 }
+
+
+# Functions for controller class.
 
 create_controller () {
-    local FILE_PATH="${PROJECT_PATH}/controller/${CONTROLLER}.java"
-    local CONTROLLER_TEMPLATE=$(get_controller_parsed)
-    touch "${FILE_PATH}"
-    echo "${CONTROLLER_TEMPLATE}" > "${FILE_PATH}"
+    local FILE_PATH=''
+    local CONTROLLER_TEMPLATE=''
+
+    FILE_PATH="${PROJECT_PATH}/controller/${CONTROLLER}.java"
+    CONTROLLER_TEMPLATE="$(get_controller_parsed)"
+
+    create_and_write_file "${FILE_PATH}" "${CONTROLLER_TEMPLATE}"
 }
 
-# FUNCTIONS FOR DTO
-
-get_dto_parsed () {
-    local DTO_TEMPLATE=$(/usr/bin/cat "${SCRIPT_PATH}"/templates/dto-template)
-    DTO_TEMPLATE=$(echo "${DTO_TEMPLATE//\{PACKAGE\}/${PACKAGE}}")
-    DTO_TEMPLATE=$(echo "${DTO_TEMPLATE//\{MODEL_NAME\}/$MODEL}")
-    echo "${DTO_TEMPLATE}"
-}
+# Functions for dto class
 
 create_dto () {
-    local FILE_PATH="${PROJECT_PATH}/model/dto/${DTO}.java"
-    local DTO_TEMPLATE=$(get_dto_parsed)
-    touch "${FILE_PATH}"
-    echo "${DTO_TEMPLATE}" > "${FILE_PATH}"
+    local FILE_PATH=''
+    local DTO_TEMPLATE=''
+
+    FILE_PATH="${PROJECT_PATH}/model/dto/${DTO}.java"
+    DTO_TEMPLATE=$(get_dto_parsed)
+
+    create_and_write_file "${FILE_PATH}" "${DTO_TEMPLATE}"
 }
 
-# FUNCTIONS FOR FACADE
+# Functions for facade class
 
-get_facade_parsed () {
-    local FACADE_TEMPLATE=$(/usr/bin/cat "${SCRIPT_PATH}"/templates/facade-template)
-    FACADE_TEMPLATE=$(echo "${FACADE_TEMPLATE//\{PACKAGE\}/${PACKAGE}}")
-    FACADE_TEMPLATE=$(echo "${FACADE_TEMPLATE//\{FACADE_CLASS_NAME\}/${FACADE}}")
-    FACADE_TEMPLATE=$(echo "${FACADE_TEMPLATE//\{MAPPER_CLASS_NAME\}/${MAPPER}}")
-    FACADE_TEMPLATE=$(echo "${FACADE_TEMPLATE//\{SERVICE_CLASS_NAME\}/${SERVICE}}")
-    FACADE_TEMPLATE=$(echo "${FACADE_TEMPLATE//\{SERVICE_VAR_NAME\}/${SERVICE,}}")
-    FACADE_TEMPLATE=$(echo "${FACADE_TEMPLATE//\{DTO_CLASS_NAME\}/${DTO}}")
-    FACADE_TEMPLATE=$(echo "${FACADE_TEMPLATE//\{MODEL_NAME\}/${MODEL}}")
-    FACADE_TEMPLATE=$(echo "${FACADE_TEMPLATE//\{MODEL_NAME_LOWER\}/${MODEL,}}")
-    FACADE_TEMPLATE=$(echo "${FACADE_TEMPLATE//\{ADD_MODEL_PARAM_CLASS_NAME\}/${ADD_PARAM}}")
-    FACADE_TEMPLATE=$(echo "${FACADE_TEMPLATE//\{UPDATE_MODEL_PARAM_CLASS_NAME\}/${UPDATE_PARAM}}")
-
-    echo "${FACADE_TEMPLATE}"
-
-}
 
 create_facade () {
-    local FILE_PATH="${PROJECT_PATH}/facade/${FACADE}.java"
-    local FACADE_TEMPLATE=$(get_facade_parsed)
-    touch "${FILE_PATH}"
-    echo "${FACADE_TEMPLATE}" > "${FILE_PATH}"
+    local FILE_PATH=''
+    local FACADE_TEMPLATE=''
+
+    FILE_PATH="${PROJECT_PATH}/facade/${FACADE}.java"
+    FACADE_TEMPLATE=$(get_facade_parsed)
+
+    create_and_write_file "${FILE_PATH}" "${FACADE_TEMPLATE}"
 }
 
-# FUNCTIONS FOR ADD PARAM
-
-get_add_param_parsed () {
-    local ADD_PARAM_TEMPLATE=$(/usr/bin/cat "${SCRIPT_PATH}"/templates/add-param-template)
-    ADD_PARAM_TEMPLATE=$(echo "${ADD_PARAM_TEMPLATE//\{PACKAGE\}/${PACKAGE}}")
-    ADD_PARAM_TEMPLATE=$(echo "${ADD_PARAM_TEMPLATE//\{ADD_PARAM_CLASS_NAME\}/${ADD_PARAM}}")
-
-    echo "${ADD_PARAM_TEMPLATE}"
-}
+# Functions for add param class
 
 create_add_param () {
-    local FILE_PATH="${PROJECT_PATH}/model/param/${ADD_PARAM}.java"
-    local ADD_PARAM_TEMPLATE=$(get_add_param_parsed)
-    touch "${FILE_PATH}"
-    echo "${ADD_PARAM_TEMPLATE}" > "${FILE_PATH}"
+    local FILE_PATH=''
+    local ADD_PARAM_TEMPLATE=''
+
+    FILE_PATH="${PROJECT_PATH}/model/param/${ADD_PARAM}.java"
+    ADD_PARAM_TEMPLATE=$(get_add_param_parsed)
+
+    create_and_write_file "${FILE_PATH}" "${ADD_PARAM_TEMPLATE}"
 }
 
-# FUNCTIONS FOR UPDATE PARAM
-
-get_update_param_parsed () {
-    local UPDATE_PARAM_TEMPLATE=$(/usr/bin/cat "${SCRIPT_PATH}"/templates/update-param-template)
-    UPDATE_PARAM_TEMPLATE=$(echo "${UPDATE_PARAM_TEMPLATE//\{PACKAGE\}/${PACKAGE}}")
-    UPDATE_PARAM_TEMPLATE=$(echo "${UPDATE_PARAM_TEMPLATE//\{UPDATE_PARAM_CLASS_NAME\}/${UPDATE_PARAM}}")
-
-    echo "${UPDATE_PARAM_TEMPLATE}"
-}
+# Functions for update param class
 
 create_update_param () {
-    local FILE_PATH="${PROJECT_PATH}/model/param/${UPDATE_PARAM}.java"
-    local UPDATE_PARAM_TEMPLATE=$(get_update_param_parsed)
-    touch "${FILE_PATH}"
-    echo "${UPDATE_PARAM_TEMPLATE}" > "${FILE_PATH}"
+    local FILE_PATH=''
+    local UPDATE_PARAM_TEMPLATE=''
+
+    FILE_PATH="${PROJECT_PATH}/model/param/${UPDATE_PARAM}.java"
+    UPDATE_PARAM_TEMPLATE=$(get_update_param_parsed)
+
+    create_and_write_file "${FILE_PATH}" "${UPDATE_PARAM_TEMPLATE}"
 }
 
-# FUNCTIONS FOR SERVICE
-
-get_service_parsed () {
-    local SERVICE_TEMPLATE=$(/usr/bin/cat "${SCRIPT_PATH}"/templates/service-template)
-    SERVICE_TEMPLATE=$(echo "${SERVICE_TEMPLATE//\{PACKAGE\}/${PACKAGE}}")
-    SERVICE_TEMPLATE=$(echo "${SERVICE_TEMPLATE//\{REPOSITORY_CLASS_NAME\}/${REPOSITORY}}")
-    SERVICE_TEMPLATE=$(echo "${SERVICE_TEMPLATE//\{REPOSITORY_VAR_NAME\}/${REPOSITORY,}}")
-    SERVICE_TEMPLATE=$(echo "${SERVICE_TEMPLATE//\{SERVICE_CLASS_NAME\}/${SERVICE}}")
-    SERVICE_TEMPLATE=$(echo "${SERVICE_TEMPLATE//\{ENTITY_CLASS_NAME\}/${MODEL}}")
-    SERVICE_TEMPLATE=$(echo "${SERVICE_TEMPLATE//\{MODEL_NAME\}/${MODEL}}")
-    SERVICE_TEMPLATE=$(echo "${SERVICE_TEMPLATE//\{MODEL_NAME_LOWER\}/${MODEL,}}")
-    SERVICE_TEMPLATE=$(echo "${SERVICE_TEMPLATE//\{ADD_MODEL_PARAM_CLASS_NAME\}/${ADD_PARAM}}")
-    SERVICE_TEMPLATE=$(echo "${SERVICE_TEMPLATE//\{UPDATE_MODEL_PARAM_CLASS_NAME\}/${UPDATE_PARAM}}")
-
-    echo "${SERVICE_TEMPLATE}"
-}
+# Functions for service class
 
 create_service () {
-    local FILE_PATH="${PROJECT_PATH}/service/${SERVICE}.java"
-    local SERVICE_TEMPLATE=$(get_service_parsed)
-    touch "${FILE_PATH}"
-    echo "${SERVICE_TEMPLATE}" > "${FILE_PATH}"
+    local FILE_PATH=''
+    local SERVICE_TEMPLATE=''
+
+    FILE_PATH="${PROJECT_PATH}/service/${SERVICE}.java"
+    SERVICE_TEMPLATE=$(get_service_parsed)
+
+    create_and_write_file "${FILE_PATH}" "${SERVICE_TEMPLATE}"
 }
 
-# FUNCTIONS FOR ENTITY
-
-get_entity_parsed () {
-    local ENTITY_TEMPLATE=$(/usr/bin/cat "${SCRIPT_PATH}"/templates/entity-template)
-
-    local ENTITY_TABLE_NAME=$(echo ${MODEL,} | sed -r 's/([a-z0-9])([A-Z])/\1_\L\2/g')
-
-    ENTITY_TEMPLATE=$(echo "${ENTITY_TEMPLATE//\{PACKAGE\}/${PACKAGE}}")
-    ENTITY_TEMPLATE=$(echo "${ENTITY_TEMPLATE//\{ENTITY_CLASS_NAME\}/${MODEL}}")
-    ENTITY_TEMPLATE=$(echo "${ENTITY_TEMPLATE//\{ENTITY_TABLE_NAME\}/${ENTITY_TABLE_NAME,}}")
-
-    echo "${ENTITY_TEMPLATE}"
-}
+# Functions for entity class
 
 create_entity () {
-    local FILE_PATH="${PROJECT_PATH}/model/entity/${MODEL}.java"
-    local ENTITY_TEMPLATE=$(get_entity_parsed)
-    touch "${FILE_PATH}"
-    echo "${ENTITY_TEMPLATE}" > "${FILE_PATH}"
+    local FILE_PATH=''
+    local ENTITY_TEMPLATE=''
+
+    FILE_PATH="${PROJECT_PATH}/model/entity/${MODEL}.java"
+    ENTITY_TEMPLATE=$(get_entity_parsed)
+
+    create_and_write_file "${FILE_PATH}" "${ENTITY_TEMPLATE}"
 }
 
-# FUNCTIONS FOR REPOSITORY
-get_repository_parsed () {
-    local REPOSITORY_TEMPLATE=$(/usr/bin/cat "${SCRIPT_PATH}"/templates/repository-template)
-    REPOSITORY_TEMPLATE=$(echo "${REPOSITORY_TEMPLATE//\{PACKAGE\}/${PACKAGE}}")
-    REPOSITORY_TEMPLATE=$(echo "${REPOSITORY_TEMPLATE//\{MODEL_NAME\}/${MODEL}}")
-
-    echo "${REPOSITORY_TEMPLATE}"
-}
+# Functions for repository class
 
 create_repository () {
-    local FILE_PATH="${PROJECT_PATH}/repository/${REPOSITORY}.java"
-    local REPOSITORY_TEMPLATE=$(get_repository_parsed)
-    touch "${FILE_PATH}"
-    echo "${REPOSITORY_TEMPLATE}" > "${FILE_PATH}"
+    local FILE_PATH=''
+    local REPOSITORY_TEMPLATE=''
+
+    FILE_PATH="${PROJECT_PATH}/repository/${REPOSITORY}.java"
+    REPOSITORY_TEMPLATE=$(get_repository_parsed)
+
+    create_and_write_file "${FILE_PATH}" "${REPOSITORY_TEMPLATE}"
 }
 
-# FUNCTIONS FOR MAPPER
-get_mapper_parsed () {
-    local MAPPER_TEMPLATE=$(/usr/bin/cat "${SCRIPT_PATH}"/templates/mapper-template)
-    MAPPER_TEMPLATE=$(echo "${MAPPER_TEMPLATE//\{PACKAGE\}/${PACKAGE}}")
-    MAPPER_TEMPLATE=$(echo "${MAPPER_TEMPLATE//\{MODEL_NAME\}/${MODEL}}")
-    MAPPER_TEMPLATE=$(echo "${MAPPER_TEMPLATE//\{DTO_CLASS_NAME\}/${DTO}}")
-    MAPPER_TEMPLATE=$(echo "${MAPPER_TEMPLATE//\{ENTITY_CLASS_NAME\}/${MODEL}}")
-    MAPPER_TEMPLATE=$(echo "${MAPPER_TEMPLATE//\{ENTITY_VAR_NAME\}/${MODEL,}}")
-
-    echo "${MAPPER_TEMPLATE}"
-}
+# Functions for mapper class
 
 create_mapper () {
-    local FILE_PATH="${PROJECT_PATH}/model/mapper/${MAPPER}.java"
-    local MAPPER_TEMPLATE=$(get_mapper_parsed)
-    touch "${FILE_PATH}"
-    echo "${MAPPER_TEMPLATE}" > "${FILE_PATH}"
+    local FILE_PATH=''
+    local MAPPER_TEMPLATE=''
+
+    FILE_PATH="${PROJECT_PATH}/model/mapper/${MAPPER}.java"
+    MAPPER_TEMPLATE=$(get_mapper_parsed)
+
+    create_and_write_file "${FILE_PATH}" "${MAPPER_TEMPLATE}"
 }
 
 create_controller
